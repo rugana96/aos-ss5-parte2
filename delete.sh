a2230cc7b0ccfdc100e3a8bb4a84dca94d3be628
Rubén Gago Navarro <r.gago@alumnos.upm.es>
2021-06-11 13:59:44 +0200

    Added K8S deployment

--- delete.sh
+++ delete.sh
@@ -8,3 +8,9 @@ kubectl delete deployment ss3
 kubectl delete deployment ss4
 kubectl delete deployment ss5
 kubectl delete deployment ss6
+kubectl delete service ss1
+kubectl delete service ss2
+kubectl delete service ss3
+kubectl delete service ss4
+kubectl delete service ss5
+kubectl delete service ss6
