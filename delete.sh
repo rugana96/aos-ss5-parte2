a541a9e5af3cba7add1f1b75e2244a77bd310e79
Rubén Gago Navarro <r.gago@alumnos.upm.es>
2021-06-11 11:02:45 +0200

    Added K8S tests

---
+++ delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl delete service taller
+kubectl delete deployment taller
+kubectl delete service tallerservice
+kubectl delete deployment ss1
+kubectl delete deployment ss2
+kubectl delete deployment ss3
+kubectl delete deployment ss4
+kubectl delete deployment ss5
+kubectl delete deployment ss6
